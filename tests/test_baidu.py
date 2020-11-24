f4db370065a019f2cb89ba2d135495153ce16995
longyang <usermryue@163.com>
2020-11-24 10:45:36 +0800

    '我的第一次提交'

---
+++ tests/test_baidu.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from selenium import webdriver
+import unittest
+import time
+
+
+class Baidu(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://www.baidu.com/"
+
+    def test_baidu_search(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_id("kw").send_keys("iTesting")
+        driver.find_element_by_id("su").click()
+        time.sleep(2)
+        search_results = driver.find_element_by_xpath('//*[@id="1"]/h3/a').get_attribute('innerHTML')
+        self.assertEqual('iTesting' in search_results, True)
+
+    @unittest.skip('i want to skip')
+    def test_baidu_set(self):
+        driver = self.driver
+        driver.get(self.base_url + "/gaoji/preferences.html")
+        m = driver.find_element_by_xpath(".//*[@id='nr']")
+        m.find_element_by_xpath("//option[@value='10']").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
